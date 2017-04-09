9bb23d3d5359da7be7ae6f3add202ad86dd2ce47
Noel Tock <hello@noeltock.com>
2017-04-09 14:33:47 +1000

    Add Gulp and Electron Reload

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const electron = require('electron-connect').server.create();
+const sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+  gulp.src('./scss/*.scss')
+    .pipe(sass({
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./css/'))
+});
+
+gulp.task('default',function() {
+  electron.start();
+  gulp.watch('./scss/*.scss',['styles']);
+  gulp.watch(['./js/*.js'], electron.restart);
+  gulp.watch(['./css/*.css'], electron.reload);
+  gulp.watch(['./index.html'], electron.reload);
+});
