73268634e27f12deffb10a9406729c95cea1a0fb
Noel Tock <hello@noeltock.com>
2017-04-09 14:14:46 +1000

    Switch to Menubar

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const {app, BrowserWindow, ipcMain, Tray, nativeImage} = require('electron')
 
 const path = require('path')
 const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow, tray
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 360, height: 430})
+  mainWindow = new BrowserWindow({
+    width: 360,
+    height: 430,
+    show: false,
+    frame: false,
+    resizable: false,
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -23,7 +25,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools(
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -32,12 +34,84 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  // Only close the window on blur if dev tools isn't opened
+
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+
+  // Get icons
+  let icon = app.getAppPath() + '/shape.png'
+  tray = new Tray(icon)
+
+  // Add a click handler so that when the user clicks on the menubar icon, it shows
+  // our popup window
+  tray.on('click', function(event) {
+    toggleWindow()
+
+    // Show devtools when command clicked
+    if (mainWindow.isVisible() && process.defaultApp && event.metaKey) {
+      mainWindow.openDevTools({mode: 'detach'})
+    }
+  })
+
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 360,
+    height: 430,
+    show: false,
+    frame: false,
+    resizable: false,
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  mainWindow.on('blur', () => {
+    if(!mainWindow.webContents.isDevToolsOpened()) {
+      mainWindow.hide()
+    }
+  })
+
+})
+
+const toggleWindow = () => {
+  if (mainWindow.isVisible()) {
+    mainWindow.hide()
+  } else {
+    showWindow()
+  }
+}
+
+const showWindow = () => {
+  const trayPos = tray.getBounds()
+  const windowPos = mainWindow.getBounds()
+  let x, y = 0
+  if (process.platform == 'darwin') {
+    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
+    y = Math.round(trayPos.y + trayPos.height)
+  } else {
+    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
+    y = Math.round(trayPos.y + trayPos.height * 10)
+  }
+
+
+  mainWindow.setPosition(x, y, false)
+  mainWindow.show()
+  mainWindow.focus()
+}
+
+ipcMain.on('show-window', () => {
+  showWindow()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
