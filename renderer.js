30fc107a8ce8dff8e408b84169e3503bc0778e8d
Pedro Freitas <dryginlogic@gmail.com>
2018-11-09 11:41:00 +0000

    Clean up and DRY range/interval conversion and storage

--- renderer.js
+++ renderer.js
@@ -16,18 +16,35 @@ const shell = require('electron').shell
 const say = require('say')
 const storage = require('electron-json-storage')
 
-// Default state
+// Slider range values in milliseconds
+const rangeValuesinMillis = [
+  1000 * 60 * 5,
+  1000 * 60 * 15,
+  1000 * 60 * 30,
+  1000 * 60 * 60,
+  1000 * 60 * 120
+]
+
+// Storage key
 const vatobeStorage = 'vatobe'
+
+// Default state
 let appState = {
   running: false,
-  interval: 900000,
+  interval: rangeValuesinMillis[1],
   speak: true,
   notification: false
 }
 
 // Persists app state to local storage
 function storeSettings () {
-  storage.set(vatobeStorage, appState, function (error) {
+  // convert app state to settings
+  var settings = {
+    intervalRange: convertMillisToRange(appState.interval),
+    speak: appState.speak,
+    notification: appState.notification
+  }
+  storage.set(vatobeStorage, settings, function (error) {
     if (error) throw error
   })
 }
@@ -36,13 +53,23 @@ function storeSettings () {
 function loadSettings () {
   storage.get(vatobeStorage, function (error, data) {
     if (error) throw error
-    // load storage into app stage
-    appState = data
+    // load storage into app state
+    var intervalRange = data.intervalRange || data.interval // for backwards compatibility
+    appState.interval = convertRangeToMillis(intervalRange)
+    appState.speak = data.speak
+    appState.notification = data.notification
     // match UI to the stored settings
-    range.value = data.interval
+    range.value = data.intervalRange
     speak.checked = data.speak
     notification.checked = data.notification
   })
+}
+
+// Resets settings UI to values in app state
+function resetSettingsUI () {
+  range.value = convertMillisToRange(appState.interval)
+  speak.checked = appState.speak
+  notification.checked = appState.notification
 }
 
 // Grab data from JSON & initialise settings
@@ -52,17 +79,14 @@ storage.has(vatobeStorage, function (error, hasKey) {
   if (hasKey) {
     loadSettings()
   } else {
-    // default UI settings
-    range.value = 2
-    speak.checked = true
-    notification.checked = false
+    resetSettingsUI()
     storeSettings()
   }
 })
 
 // Monitor settings & save data to JSON
 range.addEventListener('mouseup', function () {
-  appState.interval = convertTime(parseInt(this.value))
+  appState.interval = convertRangeToMillis(this.value)
   storeSettings()
 })
 
@@ -81,7 +105,7 @@ function fireReminder () {
   // Print initial remaining time before interval
   remainingTime(appState.interval / 1000)
 
-  // remind of posture
+  // Remind of posture
   if (appState.speak) { spokenReminder() }
   if (appState.notification) { visualReminder() }
 
@@ -90,12 +114,10 @@ function fireReminder () {
 }
 
 function spokenReminder () {
-  console.log('say.speak()')
   say.speak('Sit up straight')
 }
 
 function visualReminder () {
-  console.log('notification')
   new Notification('Sit up straight',
     {
       body: getBenefitMessage(),
@@ -122,20 +144,15 @@ function getBenefitMessage () {
   return benefitMessages[Math.floor(Math.random() * benefitMessages.length)]
 }
 
-// Interpret/lookup range values to real times
-function convertTime (option) {
-  switch (option) {
-    case 1:
-      return 1000 * 60 * 5
-    case 2:
-      return 1000 * 60 * 15
-    case 3:
-      return 1000 * 60 * 30
-    case 4:
-      return 1000 * 60 * 60
-    case 5:
-      return 1000 * 60 * 120
-  }
+// Convert milliseconds to slider range value
+function convertMillisToRange (millis) {
+  return (rangeValuesinMillis.indexOf(millis) + 1)
+}
+
+// Convert slider range value to millisecond value
+function convertRangeToMillis (option) {
+  var index = (parseInt(option) - 1)
+  return rangeValuesinMillis[index]
 }
 
 // Remaining Time
